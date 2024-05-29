286b1dbb08fe916fb668348daa13a4732b714421
c23000437 <hardiem@cardiff.ac.uk>
2024-05-29 15:03:49 +0100

    added more comments for readability

--- app/app.py
+++ app/app.py
@@ -4,18 +4,18 @@ from flask_session import Session
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileAllowed
 from wtforms import StringField, SubmitField, HiddenField, PasswordField, TextAreaField, MultipleFileField
-from wtforms.validators import ValidationError, DataRequired, Length
+from wtforms.validators import ValidationError, Length
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import update, delete
-from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required
+from flask_login import LoginManager, UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
-from werkzeug.datastructures import FileStorage
 from werkzeug.utils import secure_filename
 from PIL import Image
 from io import BytesIO
 from functools import wraps
 import datetime, base64, json, os
 
+#setting up app
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'my website password'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.sqlite3'
@@ -75,6 +75,7 @@ class Gallery(db.Model):
     description = db.Column(db.Text)
     photos = db.relationship('Photo', backref='gallery')
 
+    #used to add photo to the database
     @staticmethod
     def createGallery(userId, numPhotos, title, description):
         if userId != None:
@@ -85,6 +86,7 @@ class Gallery(db.Model):
             return gallery
         return None
 
+#declaring photo table in database
 class Photo(db.Model):
     __tablename__ = 'photos'
     __bind_key__ = 'photos'
@@ -93,6 +95,7 @@ class Photo(db.Model):
     photoURL = db.Column(db.String(32))
     thumbnailURL = db.Column(db.String(32))
 
+    #used to add photo to the database
     @staticmethod
     def addPhoto(galleryId, photoURL, thumbnailURL):
         photo = Photo(galleryId = galleryId, photoURL = photoURL, thumbnailURL = thumbnailURL)
@@ -101,10 +104,12 @@ class Photo(db.Model):
         print("Added photo")
         return photo
 
+#returns user in database from id
 @lm.user_loader
 def loadUser(id):
     return User.query.get(int(id))
 
+#returns the username from user in current session, or None if not logged in
 def getCurrentUsername():
     currentUserID = session.get("userId", None)
     currentUsername = None
@@ -112,40 +117,44 @@ def getCurrentUsername():
         currentUsername = loadUser(currentUserID).username
     return currentUsername
 
+#checks if email entered in form is valid
 def emailCheck(form, field):
     fdata = field.data
+    #must contains an '@' a '.' and be more than 5 characters
     if ('@' not in fdata) or ('.' not in fdata) or len(fdata) < 5:
         raise ValidationError("Please enter a valid email")
 
+#checks if a form field recieved contains any data
 def containsData(form, field):
     if len(field.data) == 0:
         raise ValidationError("Please fill in this field")
 
+#checks if a password entered in a form passes security checks
 def validPassword(form, field):
     fdata = field.data
+    #password must be between 6 and 15 characters and contain at least 1 uppercase letter
     if len(fdata) < 6 or len(fdata) > 15:
         raise ValidationError("Password must be between 6 and 15 characters")
     if fdata.lower() == fdata:
         raise ValidationError("Password must contain at least 1 uppercase letter")
 
+#checks if passwords sent in form for changing passwords match
 def passwordsMatch(form, field):
     if field.data != form.password1.data:
         raise ValidationError("Passwords do not match")
 
+#checks if new email to be changed is valid
 def updateEmailCheck(form, field):
     fdata = field.data
+    #must contains an '@' a '.' and be more than 5 characters
     if (('@' not in fdata) or ('.' not in fdata) or len(fdata) < 5) and len(fdata) != 0:
         raise ValidationError("Please enter a valid email")
 
+#checks if new password to be changed passes security checks
 def updatePasswordCheck(form, field):
     fdata = field.data
     if len(fdata) != 0:
-        if fdata != form.password1.data:
-            raise ValidationError("Passwords must match")
-        if len(fdata) < 6 or len(fdata) > 15:
-            raise ValidationError("Password must be between 6 and 15 characters")
-        if fdata.lower() == fdata:
-            raise ValidationError("Password must contain at least 1 uppercase letter")
+        validPassword(form, field)
 
 #checks if current user is logged into website
 def isLoggedIn():
@@ -154,6 +163,8 @@ def isLoggedIn():
         session.modified = True
     return session['loggedIn']
 
+#gets the current dictionary containing all users in database
+#refresh means will refetch info from the database
 def getUsers(refresh = False):
     if (('users' not in globals())) or (refresh == True):
         result = User.query.all()
@@ -171,6 +182,8 @@ def getUsers(refresh = False):
             }
     return users
 
+#gets the current dictionary containing all galleries in database
+#refresh means will refetch info from the database
 def getGalleries(refresh = False):
     if ('galleries' not in globals()) or (refresh == True):
         result = Gallery.query.all()
@@ -189,6 +202,8 @@ def getGalleries(refresh = False):
             }
     return galleries
 
+#gets the current dictionary containing all photos in database
+#refresh means will refetch info from the database
 def getPhotos(refresh = False):
     if ('photos' not in globals()) or (refresh == True):
         result = Photo.query.all()
@@ -203,6 +218,8 @@ def getPhotos(refresh = False):
             }
     return photos
 
+#saves an image to the server and the database
+#takes photos data, folder to be stored in and filename
 def savePhoto(data, galleryFolder, filename, photoNum):
     filePath = galleryFolder + "/" + filename
     print("Saving photo... file path = " + filePath)
@@ -238,11 +255,13 @@ def savePhoto(data, galleryFolder, filename, photoNum):
 
     return filePath, thumbnailPath
 
+#form to login to account
 class loginForm(FlaskForm):
     email = StringField("Email: ", validators = [containsData, emailCheck])
     password = PasswordField("Password: ", validators = [containsData])
     submit = SubmitField('Submit')
 
+#form to create an account
 class registerForm(FlaskForm):
     fname = StringField("First name(s): ", validators = [containsData])
     lname = StringField("Last name: ", validators = [containsData])
@@ -252,6 +271,7 @@ class registerForm(FlaskForm):
     password2 = PasswordField("Confirm your password: ", validators = [containsData, passwordsMatch])
     submit = SubmitField("Create an account")
 
+#form to change any of your user info in the database
 class settingsForm(FlaskForm):
     fname = StringField("First name:")
     lname = StringField("Last name: ")
@@ -261,6 +281,7 @@ class settingsForm(FlaskForm):
     password2 = PasswordField("Confirm your new password: ", validators = [updatePasswordCheck])
     submit = SubmitField("Update details")
 
+#form to create or change info for a gallery
 class createGalleryForm(FlaskForm):
     title = StringField('Gallery Title: ', validators=[containsData, Length(max=100)])
     description = TextAreaField('Gallery Description: ', validators=[containsData])
@@ -274,7 +295,7 @@ def homePage():
 
 @app.route("/viewGallery<int:galleryID>")
 def viewGallery(galleryID):
-    #gets info for spefic gallery
+    #gets info for spefic gallery and its photos
     gallery = getGalleries()[galleryID]
     photos = Photo.query.filter_by(galleryId=gallery['id']).all()
     URLs = {}
@@ -287,6 +308,7 @@ def viewGallery(galleryID):
 
 @app.route("/editGallery<int:galleryID>", methods = ['GET', 'POST'])
 def editGallery(galleryID):
+    #gets info about relevant gallery and its photos
     gallery = Gallery.query.filter_by(id = galleryID).first()
     oldPhotos = Photo.query.filter_by(galleryId=gallery.id).all()
     oldURLs = {}
@@ -298,21 +320,20 @@ def editGallery(galleryID):
     form = createGalleryForm()
 
     if form.validate_on_submit():
+        #sets folder for photos to be saved to on the server
         galleryFolder = (app.config['UPLOAD_FOLDER'] + "/" + str(galleryID))
-        print("Gallery folder = " + galleryFolder)
+        #gets latest gallery info from form
         newTitle = form.title.data
         newDesc = form.description.data
         photos = json.loads(form.photos.data)
-        print("Old title: " + gallery.title +"\nNewTitle:" + newTitle)
-        print("\nOld desc: " + gallery.description +"\nNew Desc:" + newDesc)
+        #if title has been changed it is updated
         if newTitle != gallery.title:
             db.session.execute(update(Gallery).where(Gallery.id == galleryID).values(title = newTitle))
             db.session.commit()
-            print("Title updated")
+        #if description has been changed it is updated
         if newDesc != gallery.description:
             db.session.execute(update(Gallery).where(Gallery.id == galleryID).values(description = newDesc))
             db.session.commit()
-            print("Description updated")
         
         #save any kept data
         for i in range (len(photos)):
@@ -341,6 +362,7 @@ def editGallery(galleryID):
         db.session.execute(update(Gallery).where(Gallery.id==galleryID).values(numPhotos = len(photos)))
         db.session.commit()
 
+        #updates dictionaries containing database info
         photos = getPhotos(True)
         global galleries
         galleries = getGalleries(True)
@@ -354,10 +376,13 @@ def editGallery(galleryID):
 def login():
     form = loginForm()
     if form.validate_on_submit():
+        #checks if user account exists
         user = User.query.filter_by(email = form.email.data.lower()).first()
+        #returns error message if account doesn't exist or password is wrong
         if user is None or not user.verify_password(form.password.data):
             return render_template('login.html', loginAttempted = True, form = form, loggedIn = False)
         else:
+            #if login right sets session info for that user and sends them to the homepage 
             session['userId'] = user.id
             session['loggedIn'] = True
             session.modified = True
@@ -366,6 +391,7 @@ def login():
 
 @app.route("/logout")
 def logout():
+    #removes user info from session to 'log them out'
     session.pop("userId")
     session['loggedIn'] = False
     session.modified = True
@@ -375,13 +401,18 @@ def logout():
 def register():
     form = registerForm()
     if form.validate_on_submit():
+        #checks if there is already an account with that email
         if User.query.filter_by(email = form.email.data).first() != None:
+            #if account already exists send error
             return render_template('register.html', form = form, invalidEmail = True)
         else:
+            #adds new user to database
             newUser = User.register(form.username.data, form.password1.data, form.fname.data, form.lname.data, form.email.data)
+            #logs user in to session
             session['userId'] = newUser.id
             session['loggedIn'] = True
             session.modified = True
+            #updates users dictionary
             global users
             users = getUsers(True)
             return redirect(url_for('homePage'))
@@ -390,23 +421,25 @@ def register():
 
 @app.route("/addGallery", methods = ['GET', 'POST'])
 def addGallery():
+    #checks if user is logged in, if not they are redirected to different page
     loggedIn = isLoggedIn()
     if not loggedIn:
         return render_template('loginNeeded.html', loggedIn = loggedIn)
     
     form = createGalleryForm()
-    if isLoggedIn():
-        username = getCurrentUsername()
-    else:
-        username = None
+    username = getCurrentUsername()
+
     if form.validate_on_submit():
-        print("valid form")
+        #gets gallery details from form
         title = form.title.data
         description = form.description.data
         photos = json.loads(form.photos.data)
+        #adds gallery to database
         newGallery = Gallery.createGallery(session.get('userId'), len(photos), title, description)
         galleryId = newGallery.id
+        #adds each photo to database and server
         for i in range (len(photos)):
+            #gets info about photo
             item = photos[i]
             filename = secure_filename(item['filename'])
             data = item['data']
@@ -414,42 +447,50 @@ def addGallery():
                 return 'No selected file or gallery ID'
             
             if item:
+                #create gallery folder
                 galleryFolder = (app.config['UPLOAD_FOLDER'] + "/" + str(galleryId))
                 if not os.path.exists(galleryFolder):
                     os.makedirs(galleryFolder)
                 
+                #save to server
                 filePath, thumbnailPath = savePhoto(data, galleryFolder, filename, i)
+                #add to database
                 newPhoto = Photo.addPhoto(galleryId, filePath, thumbnailPath)
                 
+        #updates info in database dictionaries
         photos = getPhotos(True)
         global galleries
         galleries = getGalleries(True)
         return redirect(url_for("homePage"))
     else:
-        print("Invalid form")
         return render_template("addGallery.html", loggedIn = loggedIn, form = form, currentDate = datetime.date.today(), username = username)
 
 @app.route("/myGalleries")
 def myGalleries():
     myGalls = []
+    #checks if logged in, if not redirected
     loggedIn = isLoggedIn()
     if not loggedIn:
         return render_template('loginNeeded.html', loggedIn = loggedIn)
     
+    #gets relevant galleries
     myGalls = Gallery.query.filter_by(userId=session['userId']).all()
     return render_template('myGalleries.html', loggedIn = loggedIn, photos = photos, galleries = myGalls, username = getCurrentUsername(), users = users)
 
 @app.route("/accountSettings", methods = ['GET', 'POST'])
 def settings():
+    #checks if logged in, if not redirected
     loggedIn = isLoggedIn()
     if not loggedIn:
         return render_template('loginNeeded.html', loggedIn = loggedIn)
     
     form = settingsForm()
     if form.validate_on_submit():
+        #gets current user details
         userId = session.get('userId')
         user = User.query.get(userId)
         if user:
+            #if something has been changed it is updated in the database
             if len(form.fname.data) != 0:
                 db.session.execute(update(User).where(User.id == userId).values(fname = form.fname.data))
                 db.session.commit()
@@ -465,11 +506,13 @@ def settings():
             if len(form.password1.data) != 0:
                 db.session.execute(update(User).where(User.id == userId).values(passwordHash = generate_password_hash(form.password1.data)))
                 db.session.commit()
+            #updates user dictionary as databse has been changed
             global users
             users = getUsers(True)
             return redirect(url_for('homePage'))
     return render_template("settings.html", loggedIn = loggedIn, form = form,  username = getCurrentUsername())
 
+#sets up the initial database dictionaries
 with app.app_context():
     users = getUsers()
     galleries = getGalleries()
