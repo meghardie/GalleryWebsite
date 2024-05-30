1894248e2085f5117100d5812f0cdd77a18a0e6a
c23000437 <hardiem@cardiff.ac.uk>
2024-05-30 15:27:43 +0100

    changed thumbnail size for home page

--- app/app.py
+++ app/app.py
@@ -227,28 +227,47 @@ def savePhoto(data, galleryFolder, filename, photoNum):
         data= data.split(',')[1]
 
     data = base64.b64decode(data)
-    
+    #save full image
     image = Image.open(BytesIO(data))
     image.save(filePath)
 
+    #calculate thumbnail dimensions
     thumbnail = Image.open(filePath)
     maxThumbHeight = 125
     maxThumbWidth = 125
     width = thumbnail.width
     height = thumbnail.height
     ratio = width / height
-
+    #crop thumbnail to right size
     #if photo is wider than tall then max width = 125px
     if ratio > 1:
-        width = maxThumbWidth
-        height = int(width / ratio)
-    #if photo is taller than wide max height = 125px
-    else:
         height = maxThumbHeight
         width = int(height * ratio)
-
-    # newWidth = int(thumbnailHeight * ratio)
+    #if photo is taller than wide max height = 125px
+    else:
+        width = maxThumbWidth
+        height = int(width / ratio)
     thumbnail.thumbnail((width, height))
+
+    #crop thumbnail to exact size
+    centerWidth = width//2
+    centerHeight = height//2
+    left = centerWidth - (maxThumbWidth//2)
+    right = centerWidth + (maxThumbWidth//2)
+    top = centerHeight - (maxThumbHeight//2)
+    bottom = centerHeight + (maxThumbHeight//2)
+    print(width)
+    print(height)
+    print(centerWidth)
+    print(centerHeight)
+    print(left)
+    print(centerWidth)
+    print(right)
+    print(top)
+    print(bottom)
+
+    thumbnail = thumbnail.crop((left, top, right, bottom))
+
     fileExtension = os.path.splitext(filename)[1]
     thumbnailPath = (galleryFolder + "/" + "thumbnail" + str(photoNum) + fileExtension)
     thumbnail.save(thumbnailPath)
